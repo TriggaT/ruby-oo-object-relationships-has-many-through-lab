0bb950b1704a5bab3c25dee15725fbf4e88b1a2e
Trayshawn <traytwebb14@yahoo.com>
2020-11-24 18:23:24 -0600

    commit tree 547c234dcbf8d3051a33380b757c244212341431

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,30 @@
 
+class Doctor 
+
+    attr_accessor :name, :appointment
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def new_appointment(name, patient)
+        ap = Appointment.new(name, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select {|ap| ap.doctor == self}
+    end 
+
+    def patients
+        appointments.map {|ap| ap.patient}
+    end 
+
+
+end 
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,31 @@
 
+
+class Patient 
+
+    attr_accessor :name, :appointment
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def new_appointment(date, doctor)
+        ap = Appointment.new(name, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select {|ap| ap.patient == self}
+    end 
+
+    def doctors
+        appointments.map {|ap| ap.doctor}
+    end 
+
+
+end 
